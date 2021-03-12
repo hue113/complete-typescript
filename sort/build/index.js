5d8118562ada7a04485192dd34d0aefccdea03f5
Hue Pham <huepham113@gmail.com>
2021-03-12 00:22:13 -0500

    added design patterns: sort

---
+++ sort/build/index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var NumbersCollection_1 = require("./NumbersCollection");
+// import { CharactersCollection } from "./CharactersCollection";
+// import { LinkedList } from "./LinkedList";
+var numbersCollection = new NumbersCollection_1.NumbersCollection([50, 3, -5, 0]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+// const charactersCollection = new CharactersCollection('Xaayb');
+// charactersCollection.sort();
+// console.log(charactersCollection.data);
+// const linkedList = new LinkedList();
+// linkedList.add(500);
+// linkedList.add(-10);
+// linkedList.add(-3);
+// linkedList.add(4);
+// linkedList.sort();
+// linkedList.print();
